a31fb54e4957de678af16ca9adcbca24a4de20e7
ouijajuice <eearls@ltu.edu>
2024-12-09 21:15:25 -0500

    idk

--- Assets/Scripts/LineScriptB.cs
+++ Assets/Scripts/LineScriptB.cs
@@ -8,9 +8,19 @@ public class LineScriptB : MonoBehaviour
     private Transform[] points;
     [SerializeField]
     private LineScript line;
-
+    [SerializeField] private LineRenderer lineRenderer;
+    public Color defaultColor;
+    public Color tensionColor;
     private void Start()
     {
         line.SetUpLine(points);
+        lineRenderer.SetColors(defaultColor,defaultColor);
     }
+
+    private void Update()
+    {
+        float distance = Vector2.Distance(points[0].position, points[1].position);
+        lineRenderer.SetColors(Color.Lerp(defaultColor,tensionColor,distance), Color.Lerp(defaultColor, tensionColor, distance));
+    }
+
 }
