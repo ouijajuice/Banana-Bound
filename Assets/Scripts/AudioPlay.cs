5a8dd5745b43999d2e526fd74b5123de78b1a486
ouijajuice <eearls@ltu.edu>
2024-12-12 17:17:26 -0500

    last touches

--- Assets/Scripts/AudioPlay.cs
+++ Assets/Scripts/AudioPlay.cs
@@ -11,46 +11,57 @@ public class AudioPlay : MonoBehaviour
     public AudioClip[] audioClips;
     public int clipIndex;
     public bool showSubtitle = false;
+
+    // Static flag to manage audio playing state
+    private static bool isAudioPlaying = false;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (singleAudio)
-        {
-            if (collision.gameObject.tag == tagToPlayFor)
-            {
-                this.gameObject.GetComponent<AudioSource>().Play();
-            }
-        }
-        if (multipleAudio)
-        {
-            if (collision.gameObject.tag == tagToPlayFor)
-            {
-                clipIndex = UnityEngine.Random.Range(0, audioClips.Length);
-                this.gameObject.GetComponent<AudioSource>().clip = audioClips[clipIndex];
-                this.gameObject.GetComponent<AudioSource>().Play();
-                showSubtitle = true;
-            }
-        }
+        HandleAudioTrigger(collision.gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        HandleAudioTrigger(collision.gameObject);
+    }
+
+    private void HandleAudioTrigger(GameObject triggeringObject)
+    {
+        if (triggeringObject.tag != tagToPlayFor)
+        {
+            return;
+        }
+
+        AudioSource audioSource = this.gameObject.GetComponent<AudioSource>();
+
         if (singleAudio)
         {
-            if (collision.gameObject.tag == tagToPlayFor)
+            if (!isAudioPlaying)
             {
-                this.gameObject.GetComponent<AudioSource>().Play();
+                isAudioPlaying = true;
+                audioSource.Play();
+                StartCoroutine(ResetAudioState(audioSource.clip.length));
             }
         }
+
         if (multipleAudio)
         {
-            if (collision.gameObject.tag == tagToPlayFor)
+            if (!isAudioPlaying)
             {
+                isAudioPlaying = true;
                 clipIndex = UnityEngine.Random.Range(0, audioClips.Length);
-                this.gameObject.GetComponent<AudioSource>().clip = audioClips[clipIndex];
-                this.gameObject.GetComponent<AudioSource>().Play();
+                audioSource.clip = audioClips[clipIndex];
+                audioSource.Play();
                 showSubtitle = true;
+                StartCoroutine(ResetAudioState(audioSource.clip.length));
             }
         }
+    }
+
+    private IEnumerator ResetAudioState(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        isAudioPlaying = false;
     }
 
 }
